f142da01772dfa9a309c4f38c97f522bbab9569c
Stanley Huang <huang.stanley050@gmail.com>
2019-05-23 10:37:00 +1000

    set up pages

---
+++ src/pages/attorneys.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Attorneys extends Component {
+  render() {
+    return <h1>Attorneys page</h1>;
+  }
+}
+
+export default Attorneys;
---
+++ src/pages/cases.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Cases extends Component {
+  render() {
+    return <h1>Cases Page</h1>;
+  }
+}
+
+export default Cases;
