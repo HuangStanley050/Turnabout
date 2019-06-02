7f7518e146e9b19d2b64bbe67a40c4a3f64f2dbf
Stanley Huang <huang.stanley050@gmail.com>
2019-06-02 14:43:03 +1000

    equal height for the row

--- src/components/casesComponent.js
+++ src/components/casesComponent.js
@@ -7,6 +7,7 @@ import {
   Card,
   CardImg,
   CardBody,
+  CardText,
   CardTitle,
   Button,
 } from "reactstrap";
@@ -32,8 +33,35 @@ const CasesComponent = props => {
       }
     }
   `);
-  console.log(data);
-  return null;
+  //console.log(data);
+
+  return (
+    <Container style={{ margin: "2rem auto" }}>
+      <Row className="row-eq-height">
+        {data.allContentfulCase.edges.map(({ node }) => {
+          //console.log(node.name);
+          return (
+            <Col
+              style={{ marginBottom: "1rem" }}
+              key={node.contentful_id}
+              xs="12"
+              md="4"
+            >
+              <Card style={{ height: "100%" }}>
+                <CardBody>
+                  <CardTitle>{node.name}</CardTitle>
+                  <CardText>{node.caseSummary}</CardText>
+                  <div className="text-center">
+                    <Button>More Info</Button>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 };
 
 export default CasesComponent;
