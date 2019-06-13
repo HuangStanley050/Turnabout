1c3955b317fb0dd9acd1376faae13a20ddf948b0
Stanley Huang <huang.stanley050@gmail.com>
2019-06-13 17:16:20 +1000

    added plug in to read rich text from Contentful: rich-text-react-renderer

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,12 +1,40 @@
 import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
-import { Container, Col, Row } from "reactstrap";
+import { Container, Col, Row, Button } from "reactstrap";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const BlogTemplate = ({ data }) => {
+  const {
+    title,
+    published,
+    text: { json },
+  } = data.post;
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <div>
+            <h4>The image is gone</h4>
+          </div>
+        );
+      },
+    },
+  };
+
   return (
     <Layout>
-      <h1>Hello there</h1>;
+      <section style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <h1 className="text-center mb-3">{title}</h1>
+        <h4 className="text-center mb-3">Published at: {published}</h4>
+        <article style={{ margin: "2rem 0" }}>
+          {documentToReactComponents(json, options)}
+        </article>
+        <AniLink fade to="/blog">
+          <Button className="btn-primary">Go back to Blogs</Button>
+        </AniLink>
+      </section>
     </Layout>
   );
 };
@@ -17,6 +45,9 @@ export const query = graphql`
       id
       title
       published(formatString: "MMMM DD YYYY")
+      text {
+        json
+      }
     }
   }
 `;
