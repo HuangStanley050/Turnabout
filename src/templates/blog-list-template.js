b4f9aacdccf0bb1ef40e36510d66f3bd3bce11f9
Stanley Huang <huang.stanley050@gmail.com>
2019-06-13 19:11:33 +1000

    start to work on the pagination

---
+++ src/templates/blog-list-template.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+import BlogCard from "../components/blog/blogCard";
+
+const BlogListTemplate = props => {
+  return null;
+};
+
+export default BlogListTemplate;
