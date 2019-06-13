bc06b72aecda813bcfb7381540c70f388d45eb16
Stanley Huang <huang.stanley050@gmail.com>
2019-06-13 18:24:45 +1000

    display embedded image from rich text

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -11,12 +11,14 @@ const BlogTemplate = ({ data }) => {
     published,
     text: { json },
   } = data.post;
+
   const options = {
     renderNode: {
       "embedded-asset-block": node => {
+        //console.log(node);
         return (
           <div>
-            <h4>The image is gone</h4>
+            <img width="300" src={node.data.target.fields.file["en-US"].url} />
           </div>
         );
       },
@@ -26,14 +28,16 @@ const BlogTemplate = ({ data }) => {
   return (
     <Layout>
       <section style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-        <h1 className="text-center mb-3">{title}</h1>
-        <h4 className="text-center mb-3">Published at: {published}</h4>
-        <article style={{ margin: "2rem 0" }}>
-          {documentToReactComponents(json, options)}
-        </article>
-        <AniLink fade to="/blog">
-          <Button className="btn-primary">Go back to Blogs</Button>
-        </AniLink>
+        <Container>
+          <h1 className="text-center mb-3">{title}</h1>
+          <h4 className="text-center mb-3">Published at: {published}</h4>
+          <article style={{ margin: "2rem 0" }}>
+            {documentToReactComponents(json, options)}
+          </article>
+          <AniLink fade to="/blog">
+            <Button className="btn-primary">Go back to Blogs</Button>
+          </AniLink>
+        </Container>
       </section>
     </Layout>
   );
